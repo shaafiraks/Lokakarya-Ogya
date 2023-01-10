ef75850cbc3dc8498c0d29ff6eee25d788360a68
jafars <jafarshod99@gmail.com>
2023-01-10 14:03:12 +0700

    Revert "no message"
    This reverts commit 57286baf022cf822843fddc48f36844aff3a95e1.

--- src/app/views/transaksi/transaksi.service.ts
+++ src/app/views/transaksi/transaksi.service.ts
@@ -1,21 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class TransaksiService {
-
-  baseUrl = environment.BASE_API_URL;
-
   constructor(private http: HttpClient) {}
 
   //PANGGIL API SERVICE CEK SALDO
   getNasabah(param: number): Observable<any> {
-    return this.http.get( this.baseUrl + `transaksiNasabah/cekSaldo?Nomor%20Rekening=${param}`,
+    // return this.http.get(`http://localhost:8080/transaksiNasabah/cekSaldo?Nomor%20Rekening=${param}`,
+    return this.http.get(
+      `http://localhost:8080/transaksiNasabah/cekSaldo?Nomor%20Rekening=${param}`,
       {
         responseType: 'json',
       }
@@ -24,7 +22,9 @@ export class TransaksiService {
 
   //PANGGIL API SERVICE SETOR TUNAI
   getSetorTunai(norek: number, nominal: number): Observable<any> {
-    return this.http.post( this.baseUrl +  `transaksiNasabah/setor?Nomor%20Rekening=${norek}&Nominal=${nominal}`,
+    // return this.http.post(`http://localhost:8080/transaksiNasabah/setor?Nomor%20Rekening=${norek}&Nominal=${nominal}`,
+    return this.http.post(
+      `http://localhost:8080/transaksiNasabah/setor?Nomor%20Rekening=${norek}&Nominal=${nominal}`,
       {
         responseType: 'json',
       }
@@ -33,7 +33,9 @@ export class TransaksiService {
 
   //PANGGIL API SERVICE TARIK TUNAI
   getTarikTunai(norek: number, nominal: number): Observable<any> {
-    return this.http.post(this.baseUrl + `transaksiNasabah/tarik?Nomor%20Rekening=${norek}&Nominal=${nominal}`,
+    // return this.http.post(`http://localhost:8080/transaksiNasabah/tarik?Nomor%20Rekening=${norek}&Nominal=${nominal}`,
+    return this.http.post(
+      `http://localhost:8080/transaksiNasabah/tarik?Nomor%20Rekening=${norek}&Nominal=${nominal}`,
       {
         responseType: 'json',
       }
@@ -46,7 +48,9 @@ export class TransaksiService {
     norekTujuan: number,
     nominal: number
   ): Observable<any> {
-    return this.http.post(this.baseUrl + `transaksiNasabah/transfer?Nomor%20Rekening%20Asal=${norekAsal}&Nomor%20Rekening%20Tujuan=${norekTujuan}&Nominal=${nominal}`,
+    // return this.http.post(`http://localhost:8080/transaksiNasabah/transfer?Nomor%20Rekening%20Asal=${norekAsal}&Nomor%20Rekening%20Tujuan=${norekTujuan}&Nominal=${nominal}`,
+    return this.http.post(
+      `http://localhost:8080/transaksiNasabah/transfer?Nomor%20Rekening%20Asal=${norekAsal}&Nomor%20Rekening%20Tujuan=${norekTujuan}&Nominal=${nominal}`,
       {
         responseType: 'json',
       }
@@ -54,13 +58,16 @@ export class TransaksiService {
   }
 
   getAllNasabah(): Observable<any> {
-    return this.http.get( this.baseUrl + 'masterBank/findAll', {
+    // return this.http.get("http://localhost:8080/masterBank/findAll",
+    return this.http.get('http://localhost:8080/masterBank/findAll', {
       responseType: 'json',
     });
   }
 
   getBayartelepon(noRekening: number, noTelepon: number): Observable<any> {
-    return this.http.post(this.baseUrl + `transaksiNasabah/bayarTelpon?Nomor%20Rekening=${noRekening}&No%20Telepon=${noTelepon}`,
+    // return this.http.post(`http://localhost:8080/transaksiNasabah/bayarTelpon?Nomor%20Rekening=${noRekening}&No%20Telepon=${noTelepon}`,
+    return this.http.post(
+      `http://localhost:8080/transaksiNasabah/bayarTelpon?Nomor%20Rekening=${noRekening}&No%20Telepon=${noTelepon}`,
       {
         responseType: 'json',
       }
@@ -68,7 +75,9 @@ export class TransaksiService {
   }
 
   findBayarTelepon(noRekening: number, noTelepon: number): Observable<any> {
-    return this.http.get( this.baseUrl + `transaksiNasabah/findByNoRekAndNoTelp?Nomor%20Rekening=${noRekening}&No%20Telepon=${noTelepon}`,
+    // return this.http.get(`http://localhost:8080/transaksiNasabah/findByNoRekAndNoTelp?Nomor%20Rekening=${noRekening}&No%20Telepon=${noTelepon}`,
+    return this.http.get(
+      `http://localhost:8080/transaksiNasabah/findByNoRekAndNoTelp?Nomor%20Rekening=${noRekening}&No%20Telepon=${noTelepon}`,
       {
         responseType: 'json',
       }
@@ -77,14 +86,15 @@ export class TransaksiService {
 
   //PANGGIL API SERVICE BAYAR TELEPON PERBULAN
   postBayarteleponPerBulan(noRekening: number, noTelepon:number, bulanKe:number):Observable<any>{
-    return this.http.post(this.baseUrl + `transaksiNasabah/bayarTelponPerbulan?Nomor%20Rekening=${noRekening}&No%20Telepon=${noTelepon}&Bulan%20Tagihan=${bulanKe}`,
+    return this.http.post(`http://localhost:8080/transaksiNasabah/bayarTelponPerbulan?Nomor%20Rekening=${noRekening}&No%20Telepon=${noTelepon}&Bulan%20Tagihan=${bulanKe}`,
     {
     responseType: "json",
   });
   }
 
   getTotalTagihan(noRekening: number, noTelepon: number): Observable<any> {
-    return this.http.get(this.baseUrl + `transaksiNasabah/findTotalTagihan?Nomor%20Rekening=${noRekening}&No%20Telepon=${noTelepon}`, 
+    // return this.http.get(`http://localhost:8080/transaksiNasabah/findByNoRekAndNoTelp?Nomor%20Rekening=${noRekening}&No%20Telepon=${noTelepon}`, 
+    return this.http.get(`http://localhost:8080/transaksiNasabah/findTotalTagihan?Nomor%20Rekening=${noRekening}&No%20Telepon=${noTelepon}`, 
     {
       responseType: "json",
 
